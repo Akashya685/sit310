ba41c62f27499a990bbdce6d352f5b1deb490a95
Akashya685 <156510312+Akashya685@users.noreply.github.com>
2026-05-21 22:19:58 +1000

    Update closed_loop_rotate_fast.py

--- packages/open_loop_square/src/closed_loop_rotate_fast.py
+++ packages/open_loop_square/src/closed_loop_rotate_fast.py
@@ -1,1 +1,19 @@
 
+#!/usr/bin/env python3
+
+import rospy
+from closed_loop_base import ClosedLoopBase
+
+
+class RotateFast(ClosedLoopBase):
+    def move_robot(self):
+        self.rotate_in_place(80, 2.8)
+        self.stop_robot()
+
+
+if __name__ == '__main__':
+    try:
+        duckiebot_movement = RotateFast('closed_loop_rotate_fast_node')
+        duckiebot_movement.run()
+    except rospy.ROSInterruptException:
+        pass
